615635d8ced1bec02588da573410491c671319b6
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2023-03-28 14:43:51 -0500

    Deleted submodules

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:landing/landing_screen.dart';
-import 'package:oyt_front_core/theme/theme.dart';
 
 class MyApp extends ConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -15,4 +15,76 @@ class MyApp extends ConsumerWidget {
       home: const LandingPage(),
     );
   }
+}
+
+final themeProvider = StateNotifierProvider<CustomTheme, ThemeData>((ref) {
+  return CustomTheme();
+});
+
+class CustomTheme extends StateNotifier<ThemeData> {
+  CustomTheme()
+      : super(
+          ThemeData(
+            useMaterial3: true,
+            colorSchemeSeed: Colors.deepOrange,
+            listTileTheme: const ListTileThemeData(
+              minLeadingWidth: 5,
+              horizontalTitleGap: 8,
+            ),
+            appBarTheme: const AppBarTheme(
+              systemOverlayStyle: SystemUiOverlayStyle.dark,
+              iconTheme: IconThemeData(color: Colors.black),
+              titleTextStyle: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+                color: Colors.black,
+              ),
+            ),
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+        );
+
+  void changeSeedColor(Color color) {
+    state = ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: color,
+      listTileTheme: const ListTileThemeData(
+        minLeadingWidth: 5,
+        horizontalTitleGap: 8,
+      ),
+      appBarTheme: const AppBarTheme(
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
+        iconTheme: IconThemeData(color: Colors.black),
+        titleTextStyle: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+    );
+  }
+
+  // [TEXT STYLES]
+  static const sectionTitleStyle =
+      TextStyle(fontWeight: FontWeight.w600, fontSize: 16);
+  TextStyle get selectedItemTextStyle =>
+      TextStyle(fontWeight: FontWeight.w600, color: state.primaryColor);
+  // [BOX DECORATIONS]
+  BoxDecoration get drawerBoxDecoration => BoxDecoration(
+        color: state.cardColor,
+        boxShadow: const [BoxShadow(color: Colors.black12, blurRadius: 10)],
+      );
+  BoxDecoration get roundedBoxDecoration => BoxDecoration(
+        color: state.cardColor,
+        boxShadow: const [BoxShadow(color: Colors.black12, blurRadius: 2)],
+        borderRadius: const BorderRadius.all(Radius.circular(8)),
+      );
+  // [PADDINGS]
+  static const drawerBodyPadding = EdgeInsets.symmetric(horizontal: 20);
+  static const dialogPadding =
+      EdgeInsets.symmetric(vertical: 10, horizontal: 15);
+  static const cardMargin = EdgeInsets.symmetric(vertical: 5);
+  // [COLORS]
+  static const greyColor = Color(0xff67727d);
+  static final redColor = Colors.red[700];
 }
--- lib/landing_screen.dart
+++ lib/landing_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
-import 'package:oyt_front_widgets/widgets/custom_text_field.dart';
+import 'package:flutter/services.dart';
 
 class LandingPage extends StatefulWidget {
   const LandingPage({super.key});
@@ -371,6 +371,79 @@ class IntroSection extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
+    required this.label,
+    required this.controller,
+    this.autofocus = false,
+    this.obscureText = false,
+    this.onFieldSubmitted,
+    this.validator,
+    this.decoration,
+    this.focusNode,
+    this.inputFormatters,
+    this.keyboardType,
+    this.maxLines = 1,
+    this.minLines,
+    this.textInputAction,
+    this.hintText,
+    super.key,
+    this.prefixIcon,
+    this.contentPadding,
+    this.onChanged,
+    this.enabled = true,
+    this.onTap,
+  });
+
+  final TextEditingController controller;
+  final FocusNode? focusNode;
+  final InputDecoration? decoration;
+  final TextInputType? keyboardType;
+  final TextInputAction? textInputAction;
+  final bool obscureText;
+  final bool autofocus;
+  final bool enabled;
+  final int maxLines;
+  final int? minLines;
+  final String label;
+  final Widget? prefixIcon;
+  final void Function(String)? onChanged;
+  final EdgeInsetsGeometry? contentPadding;
+  final String? hintText;
+  final List<TextInputFormatter>? inputFormatters;
+  final String? Function(String?)? validator;
+  final void Function(String?)? onFieldSubmitted;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      inputFormatters: inputFormatters,
+      onTap: onTap,
+      focusNode: focusNode,
+      keyboardType: keyboardType,
+      textInputAction: textInputAction,
+      obscureText: obscureText,
+      autofocus: autofocus,
+      onChanged: onChanged,
+      onFieldSubmitted: onFieldSubmitted,
+      enabled: enabled,
+      maxLines: maxLines,
+      minLines: minLines,
+      validator: validator,
+      decoration: decoration ??
+          InputDecoration(
+            border: const OutlineInputBorder(),
+            labelText: label,
+            prefixIcon: prefixIcon,
+            hintText: hintText,
+            contentPadding: contentPadding,
+          ),
     );
   }
 }
